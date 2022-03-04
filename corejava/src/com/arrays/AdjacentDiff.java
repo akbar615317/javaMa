21eaf03ab80847a48b0da3e8cbd000fce7d10375
EJJU <akbarmca09@gmail.com>
2022-03-05 04:39:43 +0530

    first commit

---
+++ corejava/src/com/arrays/AdjacentDiff.java
@@ -0,0 +1,32 @@
+package com.arrays;
+
+
+
+public class AdjacentDiff {
+	
+	
+	public static void main(String a[]) {
+	
+		int aa[]= {2,5,8,20,26};
+		
+		//int b[];
+		//int n=a.length-1;
+		//int temp[];
+		
+		
+	for(int i=0;i<aa.length;i++)
+	
+	{
+		
+			
+			System.out.println((Math.abs(aa[i+1])-(aa[i])));
+			
+			
+		
+	}
+		
+		
+	}
+	
+
+}
---
+++ corejava/src/com/inputsoutputs/Casting.java
@@ -0,0 +1,21 @@
+package com.inputsoutputs;
+
+public class Casting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String s=null;
+	
+		
+		System.out.println(s);
+		
+	for(int i=0;i<=10;i++)
+		{
+			
+		System.out.println(i);
+
+		}
+	}
+
+}
